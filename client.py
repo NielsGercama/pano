b1399673e6a49fd8c4bee17fbff06ad6a3923520
NielsGercama <nielsgercama@gmail.com>
2025-04-25 14:02:48 +0200

    remake for github pages

--- client.py
+++ client.py
@@ -1,6 +1,27 @@
-import requests
+import selenium
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
-url = 'https://nielsgercama.github.io/pano/myEndpoint'
-query = {'hello': "something"}
-res = requests.post(url, data=query)
-print(res.text)
+options = webdriver.ChromeOptions()
+options.add_argument("--no-sandbox")
+options.add_argument("--start-maximized")
+
+browser = webdriver.Chrome()
+browser.get('http://127.0.0.1:5000/')
+
+xinput = browser.find_element(By.ID, "x")
+yinput = browser.find_element(By.ID, "y")
+button = browser.find_element(By.ID, "submitbutton")
+
+while True:
+    x = input("x: ")
+    y = input("y: ")
+
+    if x == "exit" or y == "exit":
+        break
+
+    xinput.clear()
+    xinput.send_keys(x)
+    yinput.clear()
+    yinput.send_keys(y)
+    button.click()
